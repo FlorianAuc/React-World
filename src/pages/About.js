a21a266e551bec88779270d12bafd21446755099
FlorianAuc <florian.aucouturier@gmail.com>
2022-08-03 12:00:45 +0200

    page home and about finish

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas error
+        reprehenderit, doloremque voluptatem itaque ullam molestiae tempora
+        ducimus tempore deserunt praesentium, eligendi laudantium dolores saepe
+        ab veritatis voluptates, totam dolore! Consequatur aperiam consequuntur
+        sequi maiores quasi rerum voluptatum ducimus reiciendis porro quo
+        possimus illo modi accusantium ullam adipisci pariatur minima, ex
+        mollitia dolor facilis aliquid voluptate recusandae optio. Suscipit, sed
+        earum inventore illum ex saepe maxime provident ad exercitationem
+        quisquam et qui debitis, sapiente deleniti facilis, nam consequuntur?
+        Repudiandae id, dolorum exercitationem necessitatibus sint, nemo est
+        odio saepe perferendis eligendi ea. Quasi consequuntur inventore ea
+        aliquam nulla. Rerum, cum est!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, aut
+        recusandae? Reprehenderit nemo laborum harum culpa doloribus voluptatum
+        quae omnis sequi ipsum quia, possimus qui repellat voluptatem neque
+        laboriosam fugiat odit quaerat minima modi tenetur natus consequuntur
+        voluptate consectetur molestias? Ea natus saepe voluptatum mollitia
+        optio! Minima repellat nemo fugit iusto nulla sunt laboriosam
+        reprehenderit, nobis dolorum quod. Vero accusantium magni voluptatem
+        voluptas dolore, fuga beatae voluptatibus cumque debitis consectetur ut
+        odio aliquam, harum iste praesentium sit id in accusamus eligendi sunt
+        dicta. Alias dolorum tempora vitae amet facilis adipisci quam, eligendi
+        debitis delectus quisquam facere iste qui porro aspernatur corrupti
+        nobis natus tenetur corporis, reiciendis saepe at nam veniam! Officiis
+        officia explicabo consequatur nostrum fuga impedit dolorum ipsa qui eos
+        non iusto harum placeat possimus, ab expedita esse laudantium animi
+        error adipisci earum sed dolore ratione minus. Quae beatae harum impedit
+        officia assumenda, quos quis nulla, ab atque tempore a perspiciatis
+        fugiat magni vel dolore ea iure officiis, porro ipsa voluptate ipsum
+        error. Voluptas aliquid ab non perferendis consequatur voluptates
+        doloremque iste iure laudantium, maxime, eligendi dignissimos saepe vero
+        quisquam id neque odio modi? Eligendi corporis explicabo, placeat
+        deleniti sit labore exercitationem numquam ipsam et dignissimos sunt
+        quia! Atque.
+      </p>
+    </div>
+  );
+};
+
+export default About;
